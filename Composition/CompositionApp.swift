c2e731638c827ee55d15eac8c7136809535d5ab0
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-10-29 02:51:29 +0300

    Initial Commit

---
+++ Composition/CompositionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompositionApp.swift
+//  Composition
+//
+//  Created by Sylvan  on 29/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct CompositionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Composition/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Composition
+//
+//  Created by Sylvan  on 29/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
